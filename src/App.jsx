095176024cce7bb57e6ca104126119b029768788
Himanshu-1903 <himanshuvitthalani7@gmail.com>
2026-01-02 23:46:26 +0530

    Starting of dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 function App() {
+  const header = (
+    <header>
+      <h1>Welcome to My App</h1>
+      <h2>I am learning dev and starting with React</h2>
+      <h3>On a journey to learn full stack development</h3>
+    </header>
+  );
+
   return (
     <div>
-      Hello World
+      {header}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
